4b0e36611ee7b3d6871dbb8d1236ecf40ead9205
Ludens <me@ludens.kr>
2018-04-23 00:01:25 +0900

    prettify codes

--- main.js
+++ main.js
@@ -1,35 +1,17 @@
-/* async 아님!
-function getval(callback){
-  $.getJSON('https://rawgit.com/henrykkim/6313final/master/list.json', function(data) {
-    callback(console.log(data));
-  });
-}
-*/
 var list;
 $.ajax({
   async: false,
   dataType: 'json',
   url: "https://rawgit.com/henrykkim/6313final/master/list.json",
   type: "GET",
-  success: function(data) {
+  success: function (data) {
     console.log(data);
     list = data["restaurant"];
     return list;
   }
 });
 console.log(list);
-/*
-var restaurantList =
-  '[{"name": "9292 Korean BBQ", "filter": "Korean BBQ", "location": {"lat": 33.963723, "lng": -84.127196}}, {"name": "Han Shin Pocha USA", "filter": ["Casual Food", "Small Bites", "Karaoke"], "location": {"lat": 33.959576, "lng": -84.130336}}]';
 
-list = JSON.parse(restaurantList);
-
-console.log(list[0].location);
-
-for (i = 0; i < list.length; i++) {
-  console.log(list[i].location.lat, list[i].location.lng);
-}
-*/
 function initMap() {
   var uluru = {
     lat: 33.945208,
